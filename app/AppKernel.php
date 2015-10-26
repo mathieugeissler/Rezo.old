a1d7ba9f15123c9891da1889307eeac421fffbc5
mathieugeissler <contact@mathieugeissler.fr>
2015-10-26 08:44:09 +0100

    Maj AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,8 +17,12 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            
+            new FOS\UserBundle\FOSUserBundle(),
+            
             new Rezo\Bundle\BlogBundle\BlogBundle(),
             new Rezo\Bundle\CoreBundle\CoreBundle(),
+            new Rezo\Bundle\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
